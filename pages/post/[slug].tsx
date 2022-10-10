1c3a170004215641d5534c194175841d8bc81a4c
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-10 16:20:22 +0900

    Feat: 상세 페이지 SSG 설정
    - `getStaticPaths`로 정적 파일의 경로를 생성
    - `getStaticProps`로 Markdown 파일을 HTML로 변환해 컴포넌트에 주입
    - 전달한 HTML을 `dangerouslySetInnerHTML`을 활용해 렌더링

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,23 +1,65 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkHtml from 'remark-html';
 import React from 'react';
-import { GetStaticPaths, GetStaticProps } from 'next';
+import {
+  GetStaticPaths,
+  GetStaticProps,
+  InferGetStaticPropsType,
+  GetStaticPropsContext,
+} from 'next';
 
-type Props = {};
-
-const Post = (props: Props) => {
-  return <div>Post</div>;
+const Post = ({
+  frontmatter: { title, date, category, summary },
+  content,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  return (
+    <section>
+      <div>
+        <h1>{title}</h1>
+        <p>{summary}</p>
+        <p>{category}</p>
+        <p>{date}</p>
+      </div>
+      <div>
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+      </div>
+    </section>
+  );
 };
 
 export default Post;
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const files = fs.readdirSync(path.join('__post'));
+
+  const paths = files.map((filename) => ({
+    params: {
+      slug: filename.replace('.md', ''),
+    },
+  }));
+
   return {
-    paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
+    paths,
     fallback: false,
   };
 };
 
-export const getStaticProps: GetStaticProps = async (context) => {
+export const getStaticProps: GetStaticProps = async ({ params }: GetStaticPropsContext) => {
+  const markdownFile = fs.readFileSync(path.join('__post', params?.slug + '.md'), 'utf-8');
+
+  const { data: frontmatter, content } = matter(markdownFile);
+
+  const parsedContent = await unified().use(remarkParse).use(remarkHtml).process(content);
+
   return {
-    props: {},
+    props: {
+      frontmatter,
+      params: params?.slug,
+      content: parsedContent.value,
+    },
   };
 };

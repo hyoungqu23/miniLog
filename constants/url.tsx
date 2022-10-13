be59d590967bbdd2389d605b34215b58574243a1
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-14 00:41:48 +0900

    Feat: URL 상수화
    - 자주 활용하는 URL 데이터를 상수화해 재사용성 확보
    - 확장성을 위해 URL를 상수화하여 관리

---
+++ constants/url.tsx
@@ -0,0 +1,14 @@
+type Url = {
+  GITHUB: string;
+  NOTION: string;
+  RESUME: string;
+  PORTFOLIO: string;
+};
+
+export const URL = {
+  GITHUB: 'https://github.com/hyoungqu23',
+  NOTION:
+    'https://www.notion.so/hyoungmin/f4673e33e89f4143a0526b91d0040f89?v=38d3e22f4eb242149ed9dc21f53f7e6a',
+  RESUME: 'https://www.notion.so/hyoungmin/148ffdf1a5fe480b865ff6a0de802444',
+  PORTFOLIO: 'https://hyoungmin-portfolio.vercel.app/',
+};

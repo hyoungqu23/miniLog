e6726cc2c870c898b8aa15b1db9e00751bd5b47d
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-14 00:44:36 +0900

    Feat: Layout 공통 컴포넌트 생성 및 적용
    - Header와 Footer 적용
    - 반응형 디자인 추가
    - 가운데 정렬 및 80% width 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import 'styles/globals.css';
 import type { AppProps } from 'next/app';
+import Layout from 'components/common/layout/Layout';
+import Head from 'next/head';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>미니로그</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 };
 
 export default MyApp;

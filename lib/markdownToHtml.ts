f52abfc3dbe9931978b39e05d4ccde2f717b2173
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-14 08:37:08 +0900

    Feat: markdownToHTML 함수의 변환 과정 사이에 코드 블록 설정 추가

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,9 +1,14 @@
 import { unified } from 'unified';
 import remarkParse from 'remark-parse';
 import remarkHtml from 'remark-html';
+import prism from 'remark-prism';
 
 export const markdownToHtml = async (markdown: string) => {
-  const html = await unified().use(remarkParse).use(remarkHtml).process(markdown);
+  const html = await unified()
+    .use(remarkParse)
+    .use(remarkHtml, { sanitize: false })
+    .use(prism)
+    .process(markdown);
 
   return html;
 };

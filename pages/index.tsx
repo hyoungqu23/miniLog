2046ba65df06cb9c115a86d3e92afdf9038f2b1a
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-17 22:30:52 +0900

    Feat: Notion 데이터 getServerSideProps로 가져와 렌더링

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,43 @@
-import type { NextPage } from 'next';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import PostCard from 'components/post/PostCard';
+import type { GetServerSideProps, NextPage } from 'next';
+import { getNotionPostList } from 'services/notionApiServices';
+import { NotionPostDataType } from 'types';
 
-const Home: NextPage = () => {
-  const router = useRouter();
+type HomeProps = {
+  data: NotionPostDataType;
+};
 
-  useEffect(() => {
-    router.push('/post');
-  }, []);
-
-  return <div>MAIN</div>;
+const Home: NextPage<HomeProps> = ({ data }) => {
+  return (
+    <>
+      {data.results.map(({ properties }) => (
+        <PostCard
+          title={properties.title.title[0].plain_text}
+          slug={properties.slug.rich_text[0].plain_text}
+          date={properties.date.date.start}
+          summary={properties.summary.rich_text[0].plain_text}
+          category={properties.category.multi_select}
+        />
+      ))}
+    </>
+  );
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await getNotionPostList({
+    filter: {
+      property: 'status',
+      status: {
+        equals: 'Upload',
+      },
+    },
+  });
+
+  const data = response.data;
+
+  return {
+    props: { data },
+  };
+};

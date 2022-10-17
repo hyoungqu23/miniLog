b1f3501691f6ae2d961cb90765f3d33a5c99d631
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-17 21:47:34 +0900

    Fix: Axios의 인터셉터 제거
    - 타입 설정 이슈로 인터셉터를 제거

--- services/index.ts
+++ services/index.ts
@@ -1,23 +1,15 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { BASE_URL, TOKEN_ID } from 'config';
 
-const http = axios.create({
+const axiosConfig: AxiosRequestConfig = {
   baseURL: BASE_URL,
   headers: {
     Authorization: `Bearer ${TOKEN_ID}`,
     'Notion-Version': '2022-06-28',
     'Content-Type': 'application/json',
   },
-});
+};
 
-http.interceptors.response.use(
-  (response) => {
-    console.info('response api');
-    return response.data;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+const http = axios.create(axiosConfig);
 
 export default http;

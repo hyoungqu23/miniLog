ca5b4ef741ae9568a94be73980b14edc232b1357
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-18 16:58:54 +0900

    Feat: Notion 데이터 렌더링
    - NotionRenderer 활용
    - styling을 위한 CSS _app에 추가

---
+++ pages/[slug].tsx
@@ -0,0 +1,60 @@
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
+import React from 'react';
+import {
+  getBlockMapByPageId,
+  getNotionPostPageId,
+  getNotionPostSlugs,
+} from 'services/notionApiServices';
+import { NotionRenderer } from 'react-notion-x';
+import { ExtendedRecordMap } from 'notion-types';
+
+type PostProps = {
+  blockMap: ExtendedRecordMap;
+};
+
+const Post = ({ blockMap }: PostProps) => {
+  return (
+    <>
+      <NotionRenderer recordMap={blockMap} fullPage={true} darkMode={true} />
+    </>
+  );
+};
+
+export default Post;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const slugs = await getNotionPostSlugs({
+    filter: {
+      property: 'status',
+      status: {
+        equals: 'Upload',
+      },
+    },
+  });
+
+  return {
+    paths: slugs,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }: GetStaticPropsContext) => {
+  const slug = params?.slug as string;
+
+  const pageId = await getNotionPostPageId({
+    filter: {
+      property: 'slug',
+      rich_text: {
+        equals: slug,
+      },
+    },
+  });
+
+  const blockMap = await getBlockMapByPageId(pageId);
+
+  return {
+    props: {
+      blockMap,
+    },
+  };
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,9 @@ import 'styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from 'components/common/layout/Layout';
 import Head from 'next/head';
+import 'react-notion-x/src/styles.css';
+import 'prismjs/themes/prism-tomorrow.css';
+import 'katex/dist/katex.min.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (

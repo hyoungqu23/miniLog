c82cb0f86b5e69a3e5e63521e266d704606b3ea6
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-18 20:54:11 +0900

    Fix: 라우터 상수 변경 및 링크 추가

--- constants/routes.tsx
+++ constants/routes.tsx
@@ -1,9 +1,13 @@
 type Routes = {
   HOME: string;
   BLOG: string;
+  PORTFOLIO: string;
+  RESUME: string;
 };
 
 export const ROUTES: Routes = {
   HOME: '/',
   BLOG: '/blog',
+  PORTFOLIO: 'https://hyoungmin.vercel.app/',
+  RESUME: 'https://www.notion.so/hyoungmin/148ffdf1a5fe480b865ff6a0de802444',
 };

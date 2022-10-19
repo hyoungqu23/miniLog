96aa9a78d1503ca5e62746195eec619d0b7710a7
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-19 18:04:46 +0900

    Fix: UI 수정

--- components/common/layout/Header.tsx
+++ components/common/layout/Header.tsx
@@ -4,7 +4,7 @@ import { ROUTES } from 'constants/routes';
 
 const Header = () => {
   return (
-    <header className="sticky top-0 flex justify-center gap-5 text-2xl italic font-bold tracking-wider bg-white md:text-3xl">
+    <header className="sticky top-0 flex justify-center gap-5 text-sm italic font-bold tracking-wider bg-white dark:bg-black md:text-3xl">
       <Link href={ROUTES.HOME}>HOME</Link>
       <Link href={ROUTES.BLOG}>BLOG</Link>
       <Link href={ROUTES.PORTFOLIO}>PORTFOLIO</Link>

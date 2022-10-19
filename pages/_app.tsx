8c3ca2fc944adad76125c6c269792ab1cdf50e29
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-19 22:18:21 +0900

    Fix: 메타 태그 타이포 수정 및 뷰포트 메타 경고 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,14 @@ import Layout from 'components/common/layout/Layout';
 import 'react-notion-x/src/styles.css';
 import 'prismjs/themes/prism-tomorrow.css';
 import 'katex/dist/katex.min.css';
+import Head from 'next/head';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="viewport-fit=cover" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,17 +18,16 @@ const Document = () => (
         name="description"
         content="프론트엔드 개발자 이형민의 개발 블로그, 미니로그⭐입니다."
       />
-      <meta http-equiv="Content-Script-Type" content="Text/javascript" />
+      <meta httpEquiv="Content-Script-Type" content="Text/javascript" />
       <meta name="location" content="Seoul, Republic of Korea" />
       <meta name="robots" content="ALL" />
       <meta name="date" content="2022-10-19T18:00:00+09:00" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta http-equiv="reply-to" content="hyoungqu23@gmail.com" />
-      <meta http-equiv="email" content="hyoungqu23@gmail.com" />
-      <meta http-equiv="copyright" content="Copyright (c) 2022 Hyoungmin Lee" />
-      <meta http-equiv="publisher" content="Hyoungmin Lee" />
-      <meta http-equiv="Other Agent" content="Hyoungmin Lee" />
-      <meta http-equiv="Generator" content="Visual Studio Code" />
+      <meta httpEquiv="reply-to" content="hyoungqu23@gmail.com" />
+      <meta httpEquiv="email" content="hyoungqu23@gmail.com" />
+      <meta httpEquiv="copyright" content="Copyright (c) 2022 Hyoungmin Lee" />
+      <meta httpEquiv="publisher" content="Hyoungmin Lee" />
+      <meta httpEquiv="Other Agent" content="Hyoungmin Lee" />
+      <meta httpEquiv="Generator" content="Visual Studio Code" />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://minilog-dev.vercel.app/" />
       <meta property="og:title" content="미니로그 miniLog" />

54f8d5e9eef6156f0a0d80f9f023caac86045186
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-19 18:05:27 +0900

    Feat: 공통 메타 태그 설정

---
+++ pages/_document.tsx
@@ -0,0 +1,51 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const Document = () => (
+  <Html>
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta name="author" content="Hyoungmin Lee" />
+      <meta
+        name="keywords"
+        content="프론트엔드, 개발자, 개발 블로그, 프론트엔드 개발자, React, Next.js, Tailwind CSS, JavaScript, Redux, Notion, 미니로그"
+      />
+      <meta
+        name="subject"
+        content="프론트엔드, 개발자, 개발 블로그, 프론트엔드 개발자, React, Next.js, Tailwind CSS, JavaScript, Redux, Notion, 미니로그"
+      />
+      <meta
+        name="description"
+        content="프론트엔드 개발자 이형민의 개발 블로그, 미니로그⭐입니다."
+      />
+      <meta http-equiv="Content-Script-Type" content="Text/javascript" />
+      <meta name="location" content="Seoul, Republic of Korea" />
+      <meta name="robots" content="ALL" />
+      <meta name="date" content="2022-10-19T18:00:00+09:00" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="reply-to" content="hyoungqu23@gmail.com" />
+      <meta http-equiv="email" content="hyoungqu23@gmail.com" />
+      <meta http-equiv="copyright" content="Copyright (c) 2022 Hyoungmin Lee" />
+      <meta http-equiv="publisher" content="Hyoungmin Lee" />
+      <meta http-equiv="Other Agent" content="Hyoungmin Lee" />
+      <meta http-equiv="Generator" content="Visual Studio Code" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://minilog-dev.vercel.app/" />
+      <meta property="og:title" content="미니로그 miniLog" />
+      <meta property="og:image" content="public/main.svg" />
+      <meta
+        property="og:description"
+        content="프론트엔드 개발자 이형민의 개발 블로그, 미니로그⭐입니다."
+      />
+      <meta property="og:site_name" content="미니로그" />
+      <meta property="og:locale" content="ko_KR" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;

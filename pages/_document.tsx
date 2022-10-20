7386599915034d4ae9e9d4004e296eceed390faa
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-20 17:33:37 +0900

    Fix: 메타 태그 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -30,9 +30,9 @@ const Document = () => (
       <meta httpEquiv="Other Agent" content="Hyoungmin Lee" />
       <meta httpEquiv="Generator" content="Visual Studio Code" />
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://minilog-dev.vercel.app/" />
+      <meta property="og:url" content="https://minilog.vercel.app/" />
       <meta property="og:title" content="미니로그 miniLog" />
-      <meta property="og:image" content="main.svg" />
+      <meta property="og:image" content="main.png" />
       <meta
         property="og:description"
         content="프론트엔드 개발자 이형민의 개발 블로그, 미니로그⭐입니다."
@@ -41,6 +41,14 @@ const Document = () => (
       <meta property="og:locale" content="ko_KR" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
+      <meta property="twitter:card" content="main.png" />
+      <meta property="twitter:url" content="https://minilog.vercel.app/" />
+      <meta property="twitter:title" content="miniLog" />
+      <meta
+        property="twitter:description"
+        content="프론트엔드 개발자 이형민의 개발 블로그, 미니로그⭐입니다."
+      />
+      <meta property="twitter:image" content="main.png" />
       <link
         rel="stylesheet"
         as="style"

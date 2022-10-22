a6b8aac1d3b70808f8b47ed70f63eff1bcf348dc
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-23 00:35:59 +0900

    Fix: 이미지와 링크 태그 활용

--- components/post/PostDetail.tsx
+++ components/post/PostDetail.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import { ExtendedRecordMap } from 'notion-types';
 import { NotionRenderer } from 'react-notion-x';
+import useSWR from 'swr';
+import Image from 'next/image';
+import Link from 'next/link';
 
 import { Code } from 'react-notion-x/build/third-party/code';
 import { Collection } from 'react-notion-x/build/third-party/collection';
 import { Equation } from 'react-notion-x/build/third-party/equation';
 import { Modal } from 'react-notion-x/build/third-party/modal';
 import { Pdf } from 'react-notion-x/build/third-party/pdf';
-import useSWR, { unstable_serialize } from 'swr';
-import { useRouter } from 'next/router';
 
 type PostDetailProps = {
   data: ExtendedRecordMap;
@@ -30,6 +32,8 @@ const PostDetail = () => {
         Equation,
         Modal,
         Pdf,
+        nextImage: Image,
+        nextLink: Link,
       }}
     />
   );

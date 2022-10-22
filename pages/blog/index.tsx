dafaa0a6a94a6bc7905b1c986d6fc09d255e8c0c
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-23 00:52:41 +0900

    Feat: 목록 캐싱하기

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -3,27 +3,31 @@ import type { GetStaticProps, NextPage } from 'next';
 import { getNotionPostList } from 'services/notionApiServices';
 import { NotionPostDataType } from 'types';
 import Head from 'next/head';
+import useSWR, { unstable_serialize } from 'swr';
 
 type BlogProps = {
   data: NotionPostDataType;
 };
 
-const Blog: NextPage<BlogProps> = ({ data }) => {
+const Blog: NextPage<BlogProps> = () => {
+  const { data } = useSWR<NotionPostDataType>(['posts']);
+
   return (
     <>
       <Head>
         <title>미니로그</title>
       </Head>
-      {data.results.map(({ properties }) => (
-        <PostCard
-          key={properties.slug.rich_text[0].plain_text}
-          title={properties.title.title[0].plain_text}
-          slug={properties.slug.rich_text[0].plain_text}
-          date={properties.date.date.start}
-          summary={properties.summary.rich_text[0].plain_text}
-          category={properties.category.multi_select}
-        />
-      ))}
+      {data &&
+        data?.results.map(({ properties }) => (
+          <PostCard
+            key={properties.slug.rich_text[0].plain_text}
+            title={properties.title.title[0].plain_text}
+            slug={properties.slug.rich_text[0].plain_text}
+            date={properties.date.date.start}
+            summary={properties.summary.rich_text[0].plain_text}
+            category={properties.category.multi_select}
+          />
+        ))}
     </>
   );
 };
@@ -48,7 +52,13 @@ export const getStaticProps: GetStaticProps = async () => {
 
   const data = response.data;
 
+  console.log(data);
+
   return {
-    props: { data },
+    props: {
+      fallback: {
+        [unstable_serialize(['posts'])]: data,
+      },
+    },
   };
 };

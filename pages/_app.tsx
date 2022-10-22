523a79b4bcf15e5f4f81b2752e9c9a3058e8495f
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-22 23:40:08 +0900

    Fix: Next.js 경고에 따라 title 태그 _app.tsx로 이동

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
+        <title>miniLog</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Layout>

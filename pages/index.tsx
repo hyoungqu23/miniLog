9d27283963a11d6513969f1fd4d365a3d268a900
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-10-31 17:16:38 +0900

    Feat: 메인 화면에 이미지 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import React from 'react';
 
 const Home = () => {
@@ -10,6 +11,13 @@ const Home = () => {
       <h1 className="w-full text-lg font-semibold text-center">
         안녕하세요🖐️, 프론트엔드 개발자 이형민의 기술 블로그입니다.
       </h1>
+      <Image
+        src="/main.png"
+        className="rounded-full hover:opacity-75"
+        width="300%"
+        height="300%"
+        alt="Profile Image"
+      />
     </>
   );
 };

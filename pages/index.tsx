061ae1ce69b46749910178d17c85f7ea2efc4c9a
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2023-02-09 18:00:50 +0900

    Fix: modify existing codes to arrow function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
+import styles from '@/styles/Home.module.css';
+import { Inter } from '@next/font/google';
 import Head from 'next/head';
 import Image from 'next/image';
-import { Inter } from '@next/font/google';
-import styles from '@/styles/Home.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+const Home = () => {
   return (
     <>
       <Head>
@@ -120,4 +120,6 @@ export default function Home() {
       </main>
     </>
   );
-}
+};
+
+export default Home;
